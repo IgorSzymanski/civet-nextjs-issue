d470b3129936d4944ff7a26567e7aa9d28001f90
Igor Szymański <igor.szymanski@me.com>
2024-06-14 18:33:51 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import civetWebpackPlugin from "@danielx/civet/webpack";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "civet"],
+  webpack(config) {
+    config.plugins.unshift(civetWebpackPlugin({}));
+
+    return config;
+  },
+};
 
 export default nextConfig;
